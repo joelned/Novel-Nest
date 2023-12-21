95ec14d13da7a0278def41c634bbe48545a942d4
Joel <joelekwegh3@gmail.com>
2023-12-21 21:13:33 +0100

    user details service

---
+++ src/main/java/com/example/demo/Services/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.example.demo.Services;
+
+import com.example.demo.Models.Role;
+import com.example.demo.Models.UserEntity;
+import com.example.demo.Repositories.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String username){
+        UserEntity user = userRepository.findByName(username);
+        if(user == null){
+            throw new UsernameNotFoundException("Username not found");
+        }
+
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRole()));
+    }
+    private List<GrantedAuthority>mapRolesToAuthorities(List<Role>roles){
+        return roles.stream()
+                .map(role-> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+    }
+}
